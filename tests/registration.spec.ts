fef7946922c4c720338986caa58f31c83a68be16
Ricardo Nunes <ramosdasilvar@gmail.com>
2025-09-10 22:06:21 -0300

    Comportamento esperado

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -11,7 +11,7 @@ test.describe('teste basico', () => {
 
     await page.fill('id=input-firstname', 'Souza')
     await page.fill('id=input-lastname', 'Souza')
-    await page.fill('id=input-email', 'email2@teste.com')
+    await page.fill('id=input-email', faker.internet.email())
     await page.fill('id=input-telephone', '888777666')
     await page.fill('id=input-password', 'abc123')
     await page.fill('id=input-confirm', 'abc123')
@@ -29,7 +29,7 @@ test.describe('teste utilizando métodos built-in', () => {
 
     await page.getByLabel('First Name').fill('João')
     await page.getByLabel('Last Name').fill('Souza')
-    await page.getByLabel('E-Mail').fill('email3@teste.com')
+    await page.getByLabel('E-Mail').fill(faker.internet.email())
     await page.getByLabel('Telephone').fill('21999998888')
 
 
@@ -46,7 +46,7 @@ test.describe('teste utilizando métodos built-in', () => {
 })
 
 test.describe('teste utilizando faker', () => {
-  test.only('registrar usuário', async ({page}) => {
+  test('registrar usuário', async ({page}) => {
     await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/register')
 
     await page.fill('id=input-firstname', faker.person.firstName())
@@ -65,5 +65,39 @@ test.describe('teste utilizando faker', () => {
     await page.click('xpath=//input[@value="Continue"]')
 
     await expect(page).toHaveTitle('Your Account Has Been Created!')
+  })
+})
+
+test.describe('teste com outras validações', () => {
+  test.only('registrar usuário', async ({page}) => {
+    await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/register')
+
+    const inputFirstName = page.locator('#input-firstname')
+    await inputFirstName.fill('João')
+
+    await page.fill('id=input-firstname', 'Souza')
+    await page.fill('id=input-lastname', 'Souza')
+    await page.fill('id=input-email', faker.internet.email())
+    await page.fill('id=input-telephone', '888777666')
+    await page.fill('id=input-password', 'abc123')
+    await page.fill('id=input-confirm', 'abc123')
+    await page.click('xpath=//label[@for="input-newsletter-yes"]')
+    await page.click('xpath=//label[@for="input-agree"]')
+    await page.click('xpath=//input[@value="Continue"]')
+
+    await expect(page).toHaveTitle('Your Account Has Been Created!')
+    await expect(page).toHaveURL('https://ecommerce-playground.lambdatest.io/index.php?route=account/success')
+
+    await expect(page.locator('xpath=//div[@id="content"]/h1')).toHaveText(' Your Account Has Been Created!')
+    await expect(page.locator('xpath=//p[text()="Congratulations! Your new account has been successfully created!"]')).toBeVisible()
+    await expect(page.locator('xpath=//p[text()="You can now take advantage of member privileges to enhance your online shopping experience with us."]')).toBeVisible()
+    await expect(page.locator('xpath=//p[text()="If you have ANY questions about the operation of this online shop, please e-mail the store owner."]')).toBeVisible()
+    await expect(page.locator('xpath=//p[text()="A confirmation has been sent to the provided e-mail address. If you have not received it within the hour, please "]')).toBeVisible()
+    await expect(page.locator('xpath=//a[text()="contact us"]')).toBeVisible()
+    const coninue_button = page.locator('xpath=//a[text()="Continue"]')
+
+    await expect(coninue_button).toBeVisible()
+    await expect(coninue_button).toBeEnabled()
+
   })
 })
